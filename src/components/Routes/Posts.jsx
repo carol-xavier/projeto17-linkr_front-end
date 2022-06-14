f9f2c20d8c4d2ad69753d596fe29b00d95581426
paulo uilian <uilianlbarros@gmail.com>
2022-06-14 09:54:32 -0300

    Implementará extrura de pasta inicial

---
+++ src/components/Routes/Posts.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function Posts() {
+  return(
+    <PostContainer>
+      
+    </PostContainer>
+  );
+}
+
+export default Posts;
+
+const PostContainer = styled.section`
+  display: flex;
+`
