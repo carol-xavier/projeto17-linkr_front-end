2dac90c9a0799bd2aba98cac56a075d726444ada
Maria Luiza <maria.lmm.975@gmail.com>
2022-06-16 10:11:43 -0300

    Add UserPage

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "../../hooks/ContextAPI";
 import Posts from "../Routes/Posts";
 import HashtagPage from "../Routes/HashtagPage";
+import UserPage from "../Routes/UserPage.jsx";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Posts />} />
           <Route path="/hashtag/:hashtag" element={<HashtagPage />} />
+          <Route path="/user/:userId" element={<UserPage />} />
         </Routes>
       </BrowserRouter>
     </Provider>
