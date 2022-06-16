ba3e7519e7ef1793f89bbca6fd74e52ab258938d
paulo uilian <uilianlbarros@gmail.com>
2022-06-16 16:17:32 -0300

    Deixará o componente MainScreen mais dinamico

--- src/components/Layout/MainScreen.jsx
+++ src/components/Layout/MainScreen.jsx
@@ -1,14 +1,51 @@
+import { useEffect, useState } from "react";
+import { ThreeDots } from "react-loader-spinner";
 import styled from "styled-components";
+import { api } from "../../utils/api";
 import Header from "./Header/Header";
+import UserPost from "./Posts/UserPost";
+import TrendingBox from "./TrendingBox";
 
-function MainScreen(prop) {
-  const { children } = prop;
+function MainScreen({rote, children}) {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  function assemblyPosts(){
+    if(loading){
+      return <ThreeDots color="#fff" width={'100%'} height={'1.5rem'} />
+    }
+
+    if(posts.length === 0){
+      return <h2>There are no posts yet</h2>
+    }
+
+    return posts.map((post, id) => <UserPost key={id} postData={post} /> );
+  }
+
+  function errorGetPosts(e) {
+    setLoading(false);
+    console.log(e);
+    window.alert("An error occurred while trying to fetch the posts, please refresh the page.");
+  }
+
+  useEffect(() => {
+    api.get('/timeline')
+			.then(res => {
+        setLoading(false);
+        setPosts(res.data);
+      })
+			.catch(errorGetPosts);
+  },[])
 
   return(
     <MainScreenContainer>
       <Header />
       <main>
-        {children}
+        <Section>
+          {children}
+          {assemblyPosts()}
+        </Section>
+        <TrendingBox />
       </main>      
     </MainScreenContainer>
   );
@@ -19,7 +56,8 @@ export default MainScreen;
 const MainScreenContainer = styled.section`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  justify-content: center;
+  align-items: flex-start;
 
   width: 100%;
   height: 100%;
@@ -31,12 +69,37 @@ const MainScreenContainer = styled.section`
   
   &>main {
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    flex-direction: row;
+    justify-content: center;
+    align-items: flex-start;
 
     width: 100%;
     height: 100%;
     
     padding-inline: var(--main-screen-padding-inline);
+  }
+`
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+
+  &>h1 {
+    width: 100%;
+    padding-block: 0.8rem;
+    font-size: 1.8rem;
+    padding-left: 0;
+
+    @media (max-width: 500px) {      
+      padding-left: 0.8rem;
+    }
+  }
+
+  &>h2 {
+    padding: 0.8rem;
+    color: var(--text-color-secodary);
   }
 `
--- src/components/Routes/Home.jsx
+++ src/components/Routes/Home.jsx
@@ -5,7 +5,6 @@ import UserPost from "../Layout/Posts/UserPost";
 import { ThreeDots } from "react-loader-spinner";
 import PublishPost from "../Layout/Posts/PublishPost";
 import { api } from "../../utils/api";
-import TrendingBox from "../Layout/TrendingBox";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
@@ -40,70 +39,12 @@ function Posts() {
 
   return(
     <MainScreen >
-      <PostsContainer>
-        <section>
-          <h1>timeline</h1>
-          <PublishPost />
-          {assemblyPosts()}
-        </section>
-        <aside>
-          <TrendingBox />
-        </aside>
-      </PostsContainer>
+        <h1>timeline</h1>
+        <PublishPost />
     </MainScreen>
   );
 }
 
 export default Posts;
-
-const PostsContainer = styled.section`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: flex-start;
-  width: 100%;
-
-  &>section{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-
-    @media (min-width: 700px) {
-      width: 75%;
-    }
-    
-    h1 {
-      width: 100%;
-      padding-block: 0.8rem;
-      font-size: 1.8rem;
-      padding-left: 0;
-  
-      @media (max-width: 500px) {      
-        padding-left: 0.8rem;
-      }
-    }
-
-    &>h2 {
-      padding: 0.8rem;
-      color: var(--text-color-secodary);
-    }
-  }
-  
-  &>aside{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    
-    width: auto;
-
-    margin-left: 3rem;
-    margin-top: 3.8rem;
-
-    @media (max-width: 700px) {
-      display: none;
-    }
-  }
-`;
 
 
