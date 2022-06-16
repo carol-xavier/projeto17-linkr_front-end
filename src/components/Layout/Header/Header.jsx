f3bd0a3772511d6c93dcfd84838d8ece65798a72
Maria Luiza <maria.lmm.975@gmail.com>
2022-06-15 23:56:35 -0300

    Add SearchUser component to Header

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,32 +1,36 @@
 import styled from "styled-components";
+
 import ShowUser from "./ShowUser";
+import SearchUser from "./SearchUser";
 
 function Header() {
-
-  return (
-    <HeaderContainer>
-      <h1>linkr</h1>
-      <ShowUser />
-    </HeaderContainer>
-  )
+    return (
+        <>
+            <HeaderContainer>
+                <h1>linkr</h1>
+                <SearchUser className="desktop" />
+                <ShowUser />
+            </HeaderContainer>
+        </>
+    );
 }
 
 export default Header;
 
 const HeaderContainer = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position:relative;
 
-  width: 100%;
-  height: var(--heigth-header);
-  padding-inline: 10px;
+    width: 100%;
+    height: var(--heigth-header);
+    padding-inline: 10px;
 
-  background-color: var(--color-2);
+    background-color: var(--color-2);
 
-  &>h1 {
-    font-weight: var(--font-weight-bold);
-    font-size: 1.5rem;
-  }
-
-`
+    & > h1 {
+        font-weight: var(--font-weight-bold);
+        font-size: 1.5rem;
+    }
+`;
---
+++ src/components/Layout/Header/SearchUser.jsx
@@ -0,0 +1,106 @@
+import { DebounceInput } from "react-debounce-input";
+import axios from "axios";
+import styled from "styled-components";
+import { useState } from "react";
+
+import { getContext } from "../../../hooks/ContextAPI";
+
+export default function SearchUser() {
+    const { apiUrl } = getContext();
+    const [users, setUsers] = useState(null);
+
+    function getUsers(name) {
+        if (name.length < 3) {
+            setUsers(null);
+            return;
+        }
+        const promise = axios.get(`${apiUrl}/users/${name}`);
+        promise.then((res) => {
+            setUsers(res.data);
+        });
+        promise.catch((err) => {
+            if (err.response.status === 404) {
+                setUsers([]);
+                return;
+            }
+            console.log(err);
+            alert("Erro ao procurar usuários");
+        });
+    }
+
+    function showUsers(users) {
+        if (users.length > 0) {
+            return users.map((user) => {
+                return (
+                    <div key={user.id}>
+                        <img src={user.image} alt="" />
+                        <p>{user.name}</p>
+                    </div>
+                );
+            });
+        } else {
+            return <p>Nenhum usuário encontrado!</p>;
+        }
+    }
+
+    return (
+        <Container>
+            <DebounceInput
+                minLength={3}
+                debounceTimeout={300}
+                type="text"
+                placeholder="Search for people"
+                onChange={(e) => getUsers(e.target.value)}
+            />
+            {users ? <div>{showUsers(users)}</div> : <></>}
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    position: absolute;
+    left: calc(50% - 280px);
+    top: 13px;
+    width: 563px;
+
+    input {
+        width: 100%;
+        height: 45px;
+        border-radius: 8px;
+        padding: 14px;
+        font-size: 19px;
+    }
+
+    input::placeholder {
+        color: #c6c6c6;
+    }
+    & > div {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 14px 17px 10px;
+        border-radius: 0 0 8px 8px;
+        background-color: #e7e7e7;
+    }
+
+    div div {
+        display: flex;
+        align-items: center;
+        margin-bottom: 15px;
+    }
+
+    div div img {
+        width: 39px;
+        height: 39px;
+        margin-right: 12px;
+        object-fit: cover;
+        object-position: center;
+        background-repeat: no-repeat;
+        border-radius: 50%;
+    }
+
+    p {
+        color: #515151;
+        font-size: 19px;
+    }
+`;
