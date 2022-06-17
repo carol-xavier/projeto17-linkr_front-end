a169e026d827be7d7ec22b988930ce9e266bb044
Lucas Guarnieri <lucaslguarnieri@gmail.com>
2022-06-17 16:35:11 -0300

    Update ContextAPI.jsx

--- src/hooks/ContextAPI.jsx
+++ src/hooks/ContextAPI.jsx
@@ -4,17 +4,12 @@ const context = createContext();
 
 export function Provider(props) {
   const [ token, setToken ] = useState("");
- 
- 
-  const [refresh, setRefresh] = react.useState(false);
   
   return (
     <context.Provider
       value={{
         token,
-        setToken,
-        refresh, 
-        setRefresh
+        setToken
       }}
     >
       {props.children}
