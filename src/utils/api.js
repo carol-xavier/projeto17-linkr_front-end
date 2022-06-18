33ca71ac855db20631431db7ecfbd96051ed8377
lucas-guarnieri <lucaslguarnieri@gmail.com>
2022-06-18 16:10:53 -0300

    feat - login persistence

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 
 //TODO: change baseURL
-export const api = axios.create({baseURL: process.env.REACT_APP_API_URL});
+export const api = axios.create({baseURL:"http://localhost:5000/"});
+// export const api = axios.create({baseURL: process.env.REACT_APP_API_URL});
