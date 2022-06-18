aefb31ef41399a370a84da81773477e85657c600
Lucas Guarnieri <lucaslguarnieri@gmail.com>
2022-06-18 16:26:07 -0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,3 @@
 import axios from "axios";
 
-//TODO: change baseURL
-export const api = axios.create({baseURL:"http://localhost:5000/"});
-// export const api = axios.create({baseURL: process.env.REACT_APP_API_URL});
+export const api = axios.create({baseURL: process.env.REACT_APP_API_URL});
