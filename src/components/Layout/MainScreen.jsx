170c04a07482cc5a775018d436e1c53968887b60
paulo uilian <uilianlbarros@gmail.com>
2022-06-22 19:18:32 -0300

    Implementará mensagem de alerta sobre a ausencia de posts

--- src/components/Layout/MainScreen.jsx
+++ src/components/Layout/MainScreen.jsx
@@ -9,17 +9,18 @@ import TrendingBox from "./TrendingBox";
 
 function MainScreen({ route, children}) {
   const { header,refresh } = getContext();
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [ posts, setPosts ] = useState([]);
+  const [ followingSomeone, setFollowingSomeone ] = useState( false );
+  const [ loading, setLoading ] = useState( true );
+
+  
 
   function assemblyPosts(){
     if(loading){
       return <ThreeDots color="#fff" width={'100%'} height={'1.5rem'} />
     }
 
-    if(posts.length === 0){
-      return <h2>There are no posts yet</h2>
-    }
+    if( !posts.length && route === '/timeline' ) return infoMessage();
 
     const listOfPosts = posts.map((post, index) => 
       <UserPost key={index} postData={post} />
@@ -30,31 +31,45 @@ function MainScreen({ route, children}) {
       listOfPosts
     )
   }
+  
+  function infoMessage() {
+    if( followingSomeone ) {
+      return <h2>No posts found from your friends</h2>
+    }
+
+    return <h2>You don't follow anyone yet. Search for new friends</h2>
+  }
 
   function errorGetPosts(e) {
-    setLoading(false);
-    console.log(e);
+    setLoading( false );
+    console.log( e );
     window.alert("An error occurred while trying to fetch the posts, please refresh the page.");
   }
 
-  useEffect(() => {
-    setLoading(true);
+  function successGetPosts({ data }) {
+    const posts = data.posts || data;
+    const followingSomeone = data?.followingSomeone || false;
     
-    api.get(route, header)
-			.then(res => {
-        setLoading(false);
-        setPosts(res.data);
-      })
-			.catch(errorGetPosts);
-  },[refresh]);
+    setLoading( false );
+    setFollowingSomeone( followingSomeone );
+    setPosts( posts );
+  }
+
+  useEffect(() => {
+    setLoading( true );
+    
+    api.get( route, header)
+			.then( successGetPosts )
+			.catch( errorGetPosts );
+  },[ refresh ]);
 
   return(
     <MainScreenContainer>
       <Header />
       <main>
         <Section>
-          {children}
-          {assemblyPosts()}
+          { children }
+          { assemblyPosts() }
         </Section>
         <TrendingBox />
       </main>      
